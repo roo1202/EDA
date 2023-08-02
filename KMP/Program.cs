946e73c64752fde3f887741e584c5e0323ce34d6
Roger Fuentes Rodríguez <114238584+roo1202@users.noreply.github.com>
2023-08-02 17:06:08 -0400

    Add files via upload
    KMP algorithm

---
+++ KMP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+namespace KMP_Algorithm
+{
+    public class Program
+    {
+        public static int [] pi (string P)
+        {
+            int [] result = new int [P.Length];
+            result[0] = 0;
+            int k = 0;
+            for(int i = 1 ; i < P.Length ; i++)
+            {
+                while(k>0 && P[i] != P[k])
+                    {
+                        k = result[k-1];
+                    }
+                if(P[i] == P[k])
+                {
+                    k++;
+                }
+                result[i] = k;
+            }
+            return result;
+        }
+
+        public static List<int> KMP (string text , string p)
+        {
+            List<int> result = new List<int>();
+            int k = 0;
+            int [] Pi = new int [p.Length];
+            Pi = pi(p);
+            for(int i=0 ; i<text.Length ; i++)
+            {
+                while(k>0 && text[i] != p[k])
+                {
+                    k = Pi[k-1];
+                }
+                if(text[i] == p[k])
+                {
+                    k++;
+                    if(k == p.Length) 
+                    {
+                        result.Add(i-k+1);
+                        k = Pi[p.Length-1];
+                    }
+                }
+            } 
+            return result;
+        }
+    }
+}
